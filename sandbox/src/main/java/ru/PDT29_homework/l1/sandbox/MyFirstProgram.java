63831c449f3c66824e070b629cd2b55a4d19a568
DenisSychev <denisvsychev@gmail.com>
2016-04-14 22:05:02 +0300

    Частично изменён код.
    Изменения предложила среда разработки.

--- sandbox/src/main/java/ru/PDT29_homework/l1/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/PDT29_homework/l1/sandbox/MyFirstProgram.java
@@ -1,9 +1,8 @@
 package ru.PDT29_homework.l1.sandbox;
 
 public class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Hello, world!");
-	}
-	
 }
