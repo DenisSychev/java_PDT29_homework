10a707815f59cc5e78f1ef125dddc34b0944ea40
DenisSychev <denisvsychev@gmail.com>
2016-04-15 23:32:45 +0300

    Добавлены функции.

--- sandbox/src/main/java/ru/PDT29_homework/l1/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/PDT29_homework/l1/sandbox/MyFirstProgram.java
@@ -1,8 +1,28 @@
 package ru.PDT29_homework.l1.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("root");
 
+    double l = 445;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
+
+    int a = 43;
+    int b = 34;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " = " + area(a, b));
+  }
+
+  public static void  hello(String somebody){
+    System.out.println("Hello, " + somebody);
+  }
+
+  public static double area(Double dig){
+    return dig * dig;
+  }
+
+  public static int area (int a, int b){
+    return a * b;
+  }
 }
