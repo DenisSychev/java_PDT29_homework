c63a02913b81398299c5d9a26ed8fdf6fb64318e
DenisSychev <denisvsychev@gmail.com>
2016-04-22 21:56:47 +0300

    Корректировка названия тестового метода создания контакта.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/ContactCreationTests.java
@@ -31,7 +31,7 @@ public class ContactCreationTests {
   }
 
   @Test
-  public void PeopleCreationTests() {
+  public void testContactCreation() {
     gotoEditContactPage();
     fillContactForm(new ContactData("Имя", "И. Ф.", "Фамилия", "ФИО", "Мистер", "Компания", "Чёрная ул., Чёрный город, 1. Чёрная страна", "+559991112233", "+558881112233", "+337771112233", "+337771112332", "name@domain.zw", "last.name@domain.zw"));
     submitContactCreation();
