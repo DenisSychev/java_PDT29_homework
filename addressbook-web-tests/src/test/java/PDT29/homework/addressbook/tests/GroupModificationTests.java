8f3d855eadd5df9d9d92ba1415b2f0ee96c7d6be
DenisSychev <denisvsychev@gmail.com>
2016-04-29 23:33:59 +0300

    Добавление проверки на неизменяющиеся поля.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/GroupModificationTests.java
@@ -10,7 +10,7 @@ public class GroupModificationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("testModification1", "testModification2", "testModification3"));
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test_1.2", "test_1.3"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
