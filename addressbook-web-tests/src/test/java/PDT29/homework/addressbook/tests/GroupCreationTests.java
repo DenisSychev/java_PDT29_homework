ea7a234fe29a34cc3117a45a0c2083249d6ab59b
DenisSychev <denisvsychev@gmail.com>
2016-05-05 22:20:05 +0300

    Применение вспомогательного метода для создания группы.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/GroupCreationTests.java
@@ -8,10 +8,7 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test_1.2", "test_1.3"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createNewGroup(new GroupData("test1", "test_1.2", "test_1.3"));
   }
 
 }
