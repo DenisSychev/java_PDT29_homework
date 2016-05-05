e0b78daa8f267322c4a165f9a4bcfe2225e94216
DenisSychev <denisvsychev@gmail.com>
2016-05-05 23:37:41 +0300

    Применение вспомогательного метода для создания контакта.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactCreationTests.java
@@ -7,12 +7,10 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    app.getContactHelper().gotoEditContactPage();
-    app.getContactHelper().fillContactForm(new ContactData("Иванов", "Пётр", "Сидорович", "И. П. С.", "М.", "Компания №1",
+    app.getContactHelper().createContact(new ContactData("Иванов", "Пётр", "Сидорович", "И. П. С.", "М.", "Компания №1",
             "Светлая ул., д. 1, Светлогорск, Центральная область, Страна",
             "+08 111-22-33", "+09 222-11-33", "+06 555-66-88", null,
-            "ivanov@noone.sv", null, "test1"), true);
-    app.getContactHelper().submitContactCreation();
+            "ivanov@noone.sv", null, "test2"), true);
     app.getNavigationHelper().gotoHomepage();
   }
 
