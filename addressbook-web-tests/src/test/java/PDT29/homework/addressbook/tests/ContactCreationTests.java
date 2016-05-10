d5014b397a748efcc2730fb272dfb5b28a20f963
DenisSychev <denisvsychev@gmail.com>
2016-05-11 01:15:04 +0300

    Добавлено вычисление максимального идентификатора при создании контакта.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import PDT29.homework.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
@@ -12,13 +14,20 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() {
     app.getNavigationHelper().gotoHomepage();
     List<ContactData> before = app.getContactHelper().getContactList();
-    app.getContactHelper().createContact(new ContactData("Пётр", "Сидорович", "Иванов", "И. П. С.", "М.", "Компания №1",
+    ContactData contact = new ContactData("Фёдр", "Петрович", "Иванов", "И. П. С.", "М.", "Компания №1",
             "Светлая ул., д. 1, Светлогорск, Центральная область, Страна",
             "+08 111-22-33", "+09 222-11-33", "+06 555-66-88", null,
-            "ivanov@noone.sv", null, "test2"), true);
+            "ivanov@noone.sv", null, "test2");
+    app.getContactHelper().createContact(contact, true);
     app.getNavigationHelper().gotoHomepage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
+
+    before.add(contact);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 
 }
