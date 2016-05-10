fb426b5b1433ec90407f42d43e9f35c561db0bb8
DenisSychev <denisvsychev@gmail.com>
2016-05-11 01:11:37 +0300

    Добавлен id и методы для контакта.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/model/ContactData.java
@@ -1,6 +1,7 @@
 package PDT29.homework.addressbook.model;
 
 public class ContactData {
+  private int id;
   private final String firstName;
   private final String middleName;
   private final String lastName;
@@ -16,9 +17,10 @@ public class ContactData {
   private final String email_3;
   private String group;
 
-  public ContactData(String firstName, String middleName, String lastName, String nickName, String title, String company,
+  public ContactData(int id, String firstName, String middleName, String lastName, String nickName, String title, String company,
                      String address, String homePhone, String mobilePhone, String workPhone, String fax, String email_2,
                      String email_3, String group) {
+    this.id = id;
     this.firstName = firstName;
     this.middleName = middleName;
     this.lastName = lastName;
@@ -33,6 +35,34 @@ public class ContactData {
     this.email_2 = email_2;
     this.email_3 = email_3;
     this.group = group;
+  }
+
+  public ContactData(String firstName, String middleName, String lastName, String nickName, String title, String company,
+                     String address, String homePhone, String mobilePhone, String workPhone, String fax, String email_2,
+                     String email_3, String group) {
+    this.id = Integer.MAX_VALUE;
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickName = nickName;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.homePhone = homePhone;
+    this.mobilePhone = mobilePhone;
+    this.workPhone = workPhone;
+    this.fax = fax;
+    this.email_2 = email_2;
+    this.email_3 = email_3;
+    this.group = group;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 
   public String getFirstName() {
@@ -89,5 +119,33 @@ public class ContactData {
 
   public String getGroup() {
     return group;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "id='" + id + '\'' +
+            ", firstName='" + firstName + '\'' +
+            ", lastName='" + lastName + '\'' +
+            '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+    return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstName != null ? firstName.hashCode() : 0;
+    result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+    return result;
   }
 }
