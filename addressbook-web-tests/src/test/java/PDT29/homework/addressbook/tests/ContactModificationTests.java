e1645c95fc8e826503e1efab5b4465c0ee2fbc77
DenisSychev <denisvsychev@gmail.com>
2016-05-11 01:17:20 +0300

    Добавлено сравнение списков контактов до и после модификации.
    Неработающая версия. Предположительно сравнивается некорректное число
    индентификаторов контакта.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactModificationTests.java
@@ -4,14 +4,16 @@ import PDT29.homework.addressbook.model.ContactData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactModificationTests extends TestBase {
 
   @Test
-  public void testContactModification(){
+  public void testContactModification() {
     app.getNavigationHelper().gotoHomepage();
-    if (! app.getContactHelper().isThereAContact()){
+    if (!app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("Иванов", "Пётр", "Сидорович", "И. П. С.", "М.", "Компания №1",
               "Светлая ул., д. 1, Светлогорск, Центральная область, Страна",
               "+08 111-22-33", "+09 222-11-33", "+06 555-66-88", null,
@@ -21,13 +23,21 @@ public class ContactModificationTests extends TestBase {
     List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size() - 1);
     app.getContactHelper().initContactModification();
-    app.getContactHelper().fillContactForm(new ContactData("Name", "N.S.", "Surname", "Nickname", "Mr.", "Company",
+    ContactData contact = new ContactData(before.get(before.size() - 1).getId(), "Name", "N.S.", "Surname", "Nickname", "Mr.", "Company",
             "Black str., Black city, 1. Blackmore",
             "+666991112233", "+666881112233", "+666771112233", "+666771112332",
-            "name@domain.bm", "last.name@domain.bm", null), false);
+            "name@domain.bm", "last.name@domain.bm", null);
+    app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getNavigationHelper().gotoHomepage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(contact);
+    Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
