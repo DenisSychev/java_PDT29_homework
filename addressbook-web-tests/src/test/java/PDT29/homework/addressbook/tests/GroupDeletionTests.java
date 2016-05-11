f31856f8bb5ec0c35dea5b57754b7c44c7f0dac4
DenisSychev <denisvsychev@gmail.com>
2016-05-11 23:42:22 +0300

    Добавлено предусловие — проверка наличия группы.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/GroupDeletionTests.java
@@ -2,18 +2,23 @@ package PDT29.homework.addressbook.tests;
 
 import PDT29.homework.addressbook.model.GroupData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
 public class GroupDeletionTests extends TestBase {
 
-  @Test
-  public void testGroupDeletion() {
+  @BeforeMethod
+  public void checkPreconditions(){
     app.getNavigationHelper().gotoGroupPage();
-    if (!app.getGroupHelper().isThereAGroup()) {
+    if (! app.getGroupHelper().isThereAGroup()){
       app.getGroupHelper().createNewGroup(new GroupData("test1", "test_1.2", "test_1.3"));
     }
+  }
+
+  @Test
+  public void testGroupDeletion() {
     List<GroupData> before = app.getGroupHelper().getGroupList();
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().deleteSelectedGroups();
