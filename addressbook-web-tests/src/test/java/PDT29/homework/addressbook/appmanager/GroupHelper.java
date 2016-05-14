d98310bc1887dbd35253efd71735669201255b9a
DenisSychev <denisvsychev@gmail.com>
2016-05-14 22:56:43 +0300

    Группы. Тесты переделаны для работают со множествами.
    Добавлены методы для множеств. Удалены неиспользуемые методы и импорты.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/appmanager/GroupHelper.java
@@ -4,10 +4,10 @@ import PDT29.homework.addressbook.model.GroupData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends BaseHelper {
 
@@ -37,8 +37,8 @@ public class GroupHelper extends BaseHelper {
     click(By.name("delete"));
   }
 
-  public void selectGroup(int index) {
-    wd.findElements(By.name("selected[]")).get(index).click();
+  public void selectGroupById(int id) {
+    wd.findElement(By.xpath(".//*[@class='group']//input[@value ='" + id + "']")).click();
   }
 
   public void initGroupModification() {
@@ -56,16 +56,16 @@ public class GroupHelper extends BaseHelper {
     returnToGroupPage();
   }
 
-  public void modify(int index, GroupData group) {
-    selectGroup(index);
+  public void modify(GroupData group) {
+    selectGroupById(group.getId());
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
     returnToGroupPage();
   }
 
-  public void delete(int index) {
-    selectGroup(index);
+  public void delete(GroupData group) {
+    selectGroupById(group.getId());
     deleteSelectedGroups();
     returnToGroupPage();
   }
@@ -78,8 +78,8 @@ public class GroupHelper extends BaseHelper {
     return wd.findElements(By.xpath(".//span[@class=\"group\"]")).size();
   }
 
-  public List<GroupData> list() {
-    List<GroupData> groups = new ArrayList<GroupData>();
+  public Set<GroupData> all() {
+    Set<GroupData> groups = new HashSet<GroupData>();
     List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
     for (WebElement element : elements) {
       String name = element.getText();
@@ -89,4 +89,5 @@ public class GroupHelper extends BaseHelper {
 
     return groups;
   }
+
 }
