4737aa7933608f3999c2bcc860496ccc27f3f55e
DenisSychev <denisvsychev@gmail.com>
2016-05-17 01:52:03 +0300

    Использован метод обратных проверок.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactPhoneTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactPhoneTests.java
@@ -4,6 +4,12 @@ import PDT29.homework.addressbook.model.ContactData;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class ContactPhoneTests extends TestBase {
 
   @BeforeMethod
@@ -22,15 +28,29 @@ public class ContactPhoneTests extends TestBase {
               .withMobilePhone("+08 111-22-33")
               .withWorkPhone("+09 222-11-33")
               .withFax("+06 555-66-88")
+              .withEmail_2("bazanov@noone.sv")
               .withEmail_3("ivanov@noone.sv")
               .withGroup("Group 1"), true);
     }
   }
 
   @Test
-  public void testContactPhone(){
+  public void testContactPhone() {
     app.goTo().homepage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
+
+    assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+  }
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream().filter((s) -> !s.equals(""))
+            .map(ContactPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+  }
+
+  public static String cleaned(String phone) {
+    return phone.replaceAll("\\s", "").replaceAll("[-()]]", "");
   }
 }
