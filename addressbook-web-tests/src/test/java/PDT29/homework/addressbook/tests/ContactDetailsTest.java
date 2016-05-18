7010a6eb2cf1afa54f9bae995750e2c497aa6859
DenisSychev <denisvsychev@gmail.com>
2016-05-18 19:24:24 +0300

    Изменены сравнение и склеивание.
    Тест всё ещё не рабочий.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactDetailsTest.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactDetailsTest.java
@@ -21,16 +21,18 @@ public class ContactDetailsTest extends TestBase {
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
     ContactData contactInfoFromDetaisForm = app.contact().infoFromDetailsForm(contact);
 
-    assertThat(mergeContactInfo(contactInfoFromEditForm), equalTo(mergeDetailsContact(contactInfoFromDetaisForm)));
+    assertThat(merge(contactInfoFromDetaisForm), equalTo(mergeDetailsContact(contactInfoFromEditForm)));
+  }
+
+  private String merge(ContactData contact){
+    return Arrays.asList(contact.getAllInfo()).stream()
+            .filter((s) -> !s.equals(""))
+            .map(ContactPhoneTests::cleaned).collect(Collectors.joining("\n"));
   }
 
   private String mergeDetailsContact(ContactData contact) {
-    return Arrays.asList(contact.getTextInfo(), contact.getEmailsInfo())
-            .stream().map(ContactDetailsTest::cleaned).collect(Collectors.joining("\n"));
-  }
-
-  private String mergeContactInfo(ContactData contact) {
-    return Arrays.asList(contact.getMiddleName(),
+    return Arrays.asList(contact.getFirstName(),
+            contact.getMiddleName(),
             contact.getLastName(),
             contact.getNickName(),
             contact.getTitle(),
@@ -43,30 +45,6 @@ public class ContactDetailsTest extends TestBase {
             contact.getEmail(),
             contact.getEmail_2(),
             contact.getEmail_3())
-            .stream().map(ContactDetailsTest::cleaned).collect(Collectors.joining("\n"));
+            .stream().map(ContactPhoneTests::cleaned).collect(Collectors.joining("\n"));
   }
-
-  public static String cleaned(String info) {
-    return info.replaceAll("[()]]", "");
-  }
-
-  private String mergeContactInfoOld(ContactData contact) {
-    String allContactInfo = contact.getFirstName() + '\n' +
-            contact.getMiddleName() + '\n' +
-            contact.getLastName() + '\n' +
-            contact.getNickName() + '\n' +
-            contact.getTitle() + '\n' +
-            contact.getCompany() + '\n' +
-            contact.getAddress() + '\n' +
-            contact.getHomePhone() + '\n' +
-            contact.getMobilePhone() + '\n' +
-            contact.getWorkPhone() + '\n' +
-            contact.getFax() + '\n' +
-            contact.getEmail() + '\n' +
-            contact.getEmail_2() + '\n' +
-            contact.getEmail_3();
-    return allContactInfo;
-
-  }
-
 }
