76a270d105229ec52a0efb510e00de651f0f288e
DenisSychev <denisvsychev@gmail.com>
2016-05-19 15:33:52 +0300

    Добавлена проверка наличия контакта. Почищены импорты.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactDetailsTest.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactDetailsTest.java
@@ -2,8 +2,7 @@ package PDT29.homework.addressbook.tests;
 
 
 import PDT29.homework.addressbook.model.ContactData;
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
@@ -13,6 +12,28 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactDetailsTest extends TestBase {
+
+  @BeforeMethod
+  public void checkPreconditions() {
+    app.goTo().homepage();
+    if (app.contact().all().size() == 0) {
+      app.contact().create(new ContactData()
+              .withFirstName("Пётр")
+              .withMiddleName("Сидорович")
+              .withLastName("Базанов")
+              .withNickName("П. С. Б.")
+              .withTitle("П.")
+              .withCompany("Компания №2")
+              .withAddress("Светлая ул., д. 1, Светлогорск, Центральная область, Страна")
+              .withHomePhone("+08 111-22-33")
+              .withMobilePhone("+08 111-22-33")
+              .withWorkPhone("+09 222-11-33")
+              .withFax("+06 555-66-88")
+              .withEmail_2("bazanov@noone.sv")
+              .withEmail_3("ivanov@noone.sv")
+              .withGroup("Group 1"), true);
+    }
+  }
 
   @Test
   public void testContactDetails() {
@@ -24,7 +45,7 @@ public class ContactDetailsTest extends TestBase {
     assertThat(merge(contactInfoFromDetaisForm), equalTo(mergeDetailsContact(contactInfoFromEditForm)));
   }
 
-  private String merge(ContactData contact){
+  private String merge(ContactData contact) {
     return Arrays.asList(contact.getAllInfo()).stream()
             .filter((s) -> !s.equals(""))
             .map(ContactPhoneTests::cleaned).collect(Collectors.joining(""));
