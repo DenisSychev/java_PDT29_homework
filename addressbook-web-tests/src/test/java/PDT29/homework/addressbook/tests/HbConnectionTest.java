651562af57258b9ae4cad4258beae0e11b69c3d0
DenisSychev <denisvsychev@gmail.com>
2016-05-26 19:40:00 +0300

    Извлечение данных групп из БД технологией ORM.

---
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,43 @@
+package PDT29.homework.addressbook.tests;
+
+import PDT29.homework.addressbook.model.GroupData;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HbConnectionTest {
+
+  private SessionFactory sessionFactory;
+
+  @BeforeClass
+  protected void setUp() throws Exception {
+    // Стандартная процедура которая извлекает всю информацию о БД и о привязке классов к ней
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure() // берёт параметры из hibernate.cfg.xml
+            .build();
+    try {
+      sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    } catch (Exception e) {
+      e.printStackTrace();
+      StandardServiceRegistryBuilder.destroy(registry);
+    }
+  }
+
+  @Test
+  public void testHbConnection() {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<GroupData> result = session.createQuery("from GroupData").list();
+    for (GroupData group : result) {
+      System.out.println(group);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+}
