5ee1609215ba332b2e7d4af791b25b77809d2ad6
DenisSychev <denisvsychev@gmail.com>
2016-05-31 00:09:20 +0300

    Изменение предусловий.

--- addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactEmailTests.java
+++ addressbook-web-tests/src/test/java/PDT29/homework/addressbook/tests/ContactEmailTests.java
@@ -2,9 +2,13 @@ package PDT29.homework.addressbook.tests;
 
 
 import PDT29.homework.addressbook.model.ContactData;
+import PDT29.homework.addressbook.model.Contacts;
+import PDT29.homework.addressbook.model.GroupData;
+import PDT29.homework.addressbook.model.Groups;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
@@ -15,9 +19,24 @@ public class ContactEmailTests extends TestBase {
 
   @BeforeMethod
   public void checkPreconditions() {
-    app.goTo().homepage();
-    if (app.contact().all().size() == 0) {
-      app.contact().create(new ContactData()
+    if (app.db().groups().size() == 0) {
+      Groups before = app.db().groups();
+      app.goTo().groupPage();
+      GroupData newGroup = new GroupData()
+              .withName("test1")
+              .withHeader("test_1.2")
+              .withFooter("test_1.3");
+      app.group().create(newGroup);
+      Groups after = app.db().groups();
+      assertThat(after, equalTo(before.withAdded(newGroup.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+    }
+
+    if (app.db().contacts().size() == 0) {
+      Contacts before = app.db().contacts();
+      Groups groups = app.db().groups();
+      File photo = new File("src/test/resources/foto.jpg");
+      app.goTo().homepage();
+      ContactData newContact = new ContactData()
               .withFirstName("Пётр")
               .withMiddleName("Сидорович")
               .withLastName("Базанов")
@@ -30,8 +49,12 @@ public class ContactEmailTests extends TestBase {
               .withWorkPhone("+09 222-11-33")
               .withFax("+06 555-66-88")
               .withEmail_2("bazanov@noone.sv")
-              .withEmail_3("ivanov@noone.sv"));
-              //.withGroup("Group 1"));
+              .withEmail_3("ivanov@noone.sv")
+              .withPhoto(photo)
+              .inGroup(groups.iterator().next());
+      app.contact().create(newContact);
+      Contacts after = app.db().contacts();
+      assertThat(after, equalTo(before.withAdded(newContact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
     }
   }
 
@@ -40,7 +63,6 @@ public class ContactEmailTests extends TestBase {
     app.goTo().homepage();
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
-
     assertThat(contact.getAllEmail(), equalTo(mergeEmail(contactInfoFromEditForm)));
   }
 
