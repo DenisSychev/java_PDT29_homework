b04759bbd5f976e2db34b4216faadd552ff41552
DenisSychev <denisvsychev@gmail.com>
2016-06-01 00:45:25 +0300

    Добавлен новый модуль для тестов для Mantis.

---
+++ mantis-tests/src/test/java/PDT29/homework/mantis/tests/TestBase.java
@@ -0,0 +1,22 @@
+package PDT29.homework.mantis.tests;
+
+import PDT29.homework.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+
+  @BeforeSuite
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    app.stop();
+  }
+
+}
