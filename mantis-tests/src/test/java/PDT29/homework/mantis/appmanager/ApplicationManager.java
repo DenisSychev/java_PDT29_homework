3998301491684f60966c1fb28d641b7d665c0e42
DenisSychev <denisvsychev@gmail.com>
2016-06-01 22:44:51 +0300

    Ленивая инициализация помощников.

--- mantis-tests/src/test/java/PDT29/homework/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/PDT29/homework/mantis/appmanager/ApplicationManager.java
@@ -15,9 +15,10 @@ import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   private final Properties properties;
-  WebDriver wd;
+  private WebDriver wd;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -27,21 +28,27 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src\\test\\resources\\%s.properties", target))));
+  }
 
-    if (Objects.equals(browser, BrowserType.FIREFOX)) {
-      wd = new FirefoxDriver();
-    } else if (Objects.equals(browser, BrowserType.CHROME)) {
-      wd = new ChromeDriver();
-    } else if (Objects.equals(browser, BrowserType.IE)) {
-      wd = new InternetExplorerDriver();
+  public WebDriver getDriver() {
+    if (wd == null){
+      if (Objects.equals(browser, BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      } else if (Objects.equals(browser, BrowserType.CHROME)) {
+        wd = new ChromeDriver();
+      } else if (Objects.equals(browser, BrowserType.IE)) {
+        wd = new InternetExplorerDriver();
+      }
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
     }
-
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
+    return wd;
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null){
+      wd.quit();
+    }
   }
 
   public HttpSession newSession() {
@@ -51,4 +58,12 @@ public class ApplicationManager {
   public String getProperty(String key) {
     return properties.getProperty(key);
   }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null){
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
 }
---
+++ mantis-tests/src/test/java/PDT29/homework/mantis/appmanager/RegistrationHelper.java
@@ -0,0 +1,17 @@
+package PDT29.homework.mantis.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegistrationHelper {
+  private final ApplicationManager app;
+  private WebDriver wd;
+
+  public RegistrationHelper(ApplicationManager app) {
+    this.app = app;
+    wd = app.getDriver();
+  }
+
+  public void start(String username, String email) {
+    wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+  }
+}
