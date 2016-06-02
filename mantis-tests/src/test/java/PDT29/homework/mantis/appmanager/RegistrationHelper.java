2eda5f2661c40d5bc4ac868226d1aefe38d8f8be
DenisSychev <denisvsychev@gmail.com>
2016-06-03 00:23:35 +0300

    RegistrationHelper теперь работает через BaseHelper.

--- mantis-tests/src/test/java/PDT29/homework/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/PDT29/homework/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,25 @@
 package PDT29.homework.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends  BaseHelper{
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app = app;
-    wd = app.getDriver();
+    super(app);
   }
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl") + "/signup_page.php");
+    type(By.name("username"), username);
+    type(By.name("email"), email);
+    click(By.xpath(".//input[@value='Signup']"));
+  }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.name("password"), password);
+    type(By.name("password_confirm"), password);
+    click(By.xpath(".//input[@value='Update User']"));
   }
 }
