f0e7ffaaff673f71040347fb9e3f3b5a4b74486a
DenisSychev <denisvsychev@gmail.com>
2016-06-03 00:24:16 +0300

    Регистрация нового пользователя с проверкой подтверждения через почту.

---
+++ mantis-tests/src/test/java/PDT29/homework/mantis/model/MailMessage.java
@@ -0,0 +1,12 @@
+package PDT29.homework.mantis.model;
+
+public class MailMessage {
+
+  public String to;
+  public String text;
+
+  public MailMessage (String to, String text) {
+    this.to = to;
+    this.text = text;
+  }
+}
